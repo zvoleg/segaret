2501774f72e4f5a755c802f49d4e63bc443f54a0
zvoleg <oleg.zaitsevv@gmail.com>
2025-02-08 17:31:55 +0300

    correct reading of memory by vdp bus

--- src/vdp_bus.rs
+++ src/vdp_bus.rs
@@ -19,15 +19,16 @@ impl VdpBus {
 impl BusVdp for VdpBus {
     fn read(&self, address: u32) -> u16 {
         debug!("VDP reads address {:08X}", address);
-        unsafe {
+        let data = unsafe {
             if address < 0x400000 {
-                *(&self.memory_space.borrow().rom[address as usize] as *const _ as *const u16)
+                *(self.memory_space.borrow().rom.as_ptr().offset(address as isize) as *const _ as *const u16)
             } else if address >= 0xFF0000 {
                 let address = address & 0xFFFF;
-                *(&self.memory_space.borrow().m68k_ram[address as usize] as *const _ as *const u16)
+                *(self.memory_space.borrow().m68k_ram.as_ptr().offset(address as isize) as *const _ as *const u16)
             } else {
                 panic!("VdpBus: read: wrong address value: {:08X}", address)
             }
-        }
+        };
+        data
     }
 }
